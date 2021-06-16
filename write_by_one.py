bb51c41657bc0c2373e75de315c9afced5fd0ebc
09python <83383530+09python@users.noreply.github.com>
2021-06-16 13:31:07 +0800

    Update write_by_one.py

--- write_by_one.py
+++ write_by_one.py
@@ -1,34 +1,6 @@
-##from win32com.client import Dispatch
-##
-##xlApp = Dispatch('Excel.Application')
-##xlApp.Visible = True
-##
-##xlBook = xlApp.Workbooks.Open(r'C:\Users\021\Desktop\123\123.xlsx')
-##
-##for row in range(200000):
-##        for col in range(0,200000):
-##            xlBook.write(row + 1, col, col)
-##   # file.save('123.xlsx')
-##
-###xlBook.Save()
-##
-###xlBook.Close()
-##
-###xlApp.Quit()
-
-##import xlwt
-##book=xlwt.Workbook(encoding='utf-8',style_compression=0)
-##sheet=book.add_sheet('date',cell_overwrite_ok=True)
-##for i in range(65536):
-##        sheet.write(i,0,'1234')
-##
-##
-##book.save(r'C:\Users\021\Desktop\123\7.xls')
-##
-
 import openpyxl
 
-file = openpyxl.Workbook()  #创建一个工作簿,W大写
+file = openpyxl.Workbook()  #创建一个工作簿
 
 print(file.sheetnames)     #新建文档默认存在一个Sheet
 
